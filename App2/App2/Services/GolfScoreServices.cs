565b10f00c3bc4c1b66448af185fd874bde0f087
unknown <josh.zollinger@nwciowa.edu>
2019-04-11 04:01:56 -0500

    Added MVVM structure, with base classes

---
+++ App2/App2/Services/GolfScoreServices.cs
@@ -0,0 +1,23 @@
+﻿using App2.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.Services
+{
+    public class GolfScoreServices
+    {
+        public List<GolfScore> GetGolfScores()
+        {
+            var list = new List<GolfScore>
+            {
+                new GolfScore
+                {
+                    Hole1 = 3,
+                    Hole2 = 4
+                }
+            };
+            return list;
+        }
+    }
+}
---
+++ App2/App2/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using App2.Models;
+using App2.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace App2.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private List<GolfScore> golfScoresList;
+
+        public List<GolfScore> GolfScoresList
+        {
+            get { return golfScoresList; }
+            set
+            {
+                golfScoresList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            var GolfScoreServices = new GolfScoreServices();
+
+            GolfScoresList = GolfScoreServices.GetGolfScores();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
