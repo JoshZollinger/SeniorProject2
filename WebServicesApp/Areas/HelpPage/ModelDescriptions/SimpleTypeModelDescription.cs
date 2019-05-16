99aaeb7128666cd21c81119670b4a8d430977c11
unknown <josh.zollinger@nwciowa.edu>
2019-05-15 20:41:38 -0500

    Deleted old APIs

--- WebServicesApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebServicesApp.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- WebServicesApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebServicesApp.WebApiApplication" Language="C#" %>
